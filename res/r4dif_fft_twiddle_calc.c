a8b35866e2d96936716cd07f89e5c22cc7d0c8d9
JamesNorris <JNorris700@gmail.com>
2018-03-20 22:45:40 -0400

    Attempted fix for r4f twiddle coefficient calculation.

--- res/r4dif_fft_twiddle_calc.c
+++ res/r4dif_fft_twiddle_calc.c
@@ -8,45 +8,24 @@ CREATED BY: James C. Norris
 
 #include <stdio.h>
 #include <stdlib.h>
-//#include <string.h>
-//#include <inttypes.h>
 #include <math.h>
 
 #define PI 3.14159265359
 
-struct complex {
-	int real, imag;
-};
-
-double _log4(double d) {
-	return (log(d) / log(4));
-}
-
-struct complex* calc(double k, double n, int N) {
-	struct complex* c = malloc(sizeof(struct complex));
-	
-	double phase = 2 * PI * (k * n) / N;
-	
-	c->real = (int) cos(phase);
-	c->imag = (int) -sin(phase);
-	
-	return c;
-}
-
 int main(int argc, char* argv[]) {
 	int N = atoi(argv[1]);
-	int stages = _log4((double) N + 1 /* +1 to avoid precision bug */);
-	int factors = N / 4;
 	
-	printf("...\nFinding twiddle coefficients for:\nSAMPLES(N)=%i\nSTAGES(Pmax)=%i\
-	\nFACTORS(kmax)=%i\n...\n", N, stages, factors);
+	printf("...\nFinding twiddle coefficients for:\nSAMPLES(N)=%i\n...\n", N);
 	
-	for (double k = 0; k < factors; k++) {
+	for (double k = 0; k < N; k++) {
 		for (double n = 0; n < N; n++) {
 			
-			struct complex* c = calc(k, n, N);
+			double phase = 2 * PI * (k * n) / N;
 			
-			printf("T(k:%i, n:%i) = %i + %ij\n", (int) k, (int) n, c->real, c->imag);
+			int real = (int) cos(phase);
+			int imag = (int) -sin(phase);
+			
+			printf("T(k:%i, n:%i) = %i + %ij\n", (int) k, (int) n, real, imag);
 			
 		}
 	}
